9b38975d49dcb3da5b4352d4fde9d1d7627adbb9
Felipe Reis <coder.felipe@gmail.com>
2018-02-07 20:38:28 -0200

    Criação do banco mysql

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,18 @@
 module.exports = function(app){
+
     app.get('/noticias', function(req, res){
-        res.render("noticias/noticias");
+        
+        var mysql = require('mysql');
+        
+        var con = mysql.createConnection({
+            host: '127.0.0.1',
+            user: 'root',
+            password: 'Flp@123',
+            database: 'portal_noticias'
+        });
+
+        con.query('select * from noticias',function(error, result){
+            res.render("noticias/noticias", {noticias : result});
+        });  
     });
 };
