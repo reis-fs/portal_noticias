5b842a3d44516b40eb6de5d80d21874c472cca05
Felipe Reis <coder.felipe@gmail.com>
2018-02-28 02:09:50 -0300

    ajustes no codigo

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,12 +1,12 @@
 module.exports = function(app){
-    
+
     app.get('/noticias', function(req, res){
 
-        var con = app.config.dbConnection(); 
+        var con = app.config.dbConnection();
         var noticiasModel = new app.app.models.NoticiasDAO(con);
 
         noticiasModel.getNoticias(function(error, result){
             res.render("noticias/noticias", {noticias : result});
-        });  
-    }); 
+        });
+    });
 };
